2790bc1db854b69cf28a83d84247e49c3665dc08
ARMAAN199 <56539435+ARMAAN199@users.noreply.github.com>
2021-12-01 10:55:46 +0530

    new

---
+++ Coldplay.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <iterator>
+#include <set>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n,a;
+        cin >> n >> a;
+        cout << n/a << endl;
+
+	}
+	return 0;
+}
